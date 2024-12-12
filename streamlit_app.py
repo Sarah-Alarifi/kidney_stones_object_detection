489ee0a985a73122981a7edf9c3cb4a68f3c8d78
sarah <133251713+Sarah-Alarifi@users.noreply.github.com>
2024-12-12 19:02:57 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,32 @@
 import streamlit as st
+from PIL import Image
+import numpy as np
+from ultralytics import YOLO 
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("Kidney Stones YOLO Object Detection")
+st.write("Upload an image, and let the YOLO model detect objects in it.")
+
+@st.cache_resource
+def load_model():
+  
+    model = YOLO("kidney_yolo.pt") 
+    return model
+
+model = load_model()
+
+uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+
+if uploaded_file is not None:
+    # Display uploaded image
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.write("Detecting objects...")
+
+    image_np = np.array(image)
+
+    results = model.predict(image_np)
+
+    st.image(results[0].plot(), caption="Detection Results", use_column_width=True)
+
+    st.write("Detection Results:")
+    st.write(results[0].boxes.data.cpu().numpy()) 
